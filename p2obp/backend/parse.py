84a410c7da6538550327ca2e04763f5f065f74f9
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-04-27 14:32:40 +0200

    Add functionality to `parse`

--- p2obp/backend/parse.py
+++ p2obp/backend/parse.py
@@ -22,27 +22,130 @@ Example of usage:
     ... {'run 5, 109.2313.005 = 0109.C-0413(E)': {'nights 2-4: {'SCI': ['MY Lup', ...],
     ...  'CAL': [['HD142198'], ...], 'TAG': [['LN'], ...]}}}
 """
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from .utils import prompt_user
+
+
 # TODO: Make parser accept more than one calibrator block for one night, by
 # checking if there are integers for numbers higher than last calibrator and
 # then adding these
-# TODO: Improve the documentation and the docstrings
-
-from pathlib import Path
-from typing import Any, Dict, List, Optional
-
-from creator import read_dict_to_lists
 
 
-def parse_line(parts: str) -> str:
-    """Parses a line from the `calibrat_find`-tool and returns the objects name
+# TODO: Write down how the nights need to be written down in the observing plan
+def parse_night_name(night_name: str) -> str:
+    """Automatically gets the night's date from a night key of
+    the dictionary if the date it is included in the key.
 
     Parameters
     ----------
-    parts: str
+    night_key : str
+        The dictionary's keys that describes a night.
 
     Returns
     -------
-    target_name: str
+    night_name : str
+        If night date in night then of the format <night>_<night_date> if not
+        then <night>.
+    """
+    if "full" in night_name:
+        return night_name
+
+    night = night_name.split(":")[0].strip()
+    date = night_name.split(":")[1].split(",")[0].strip()
+
+    if len(night.split()) > 2:
+        try:
+            night, date = night.split(",")[:2]
+        except ValueError:
+            return night
+
+    return "_".join([''.join(night.split()), ''.join(date.split())])\
+        if date != '' else ''.join(night.split())
+
+
+def parse_array_config(run_name: Optional[str] = None) -> str:
+    """Parses the array configuration from string containing it.
+
+    If no run name is specified or no match can be found
+    it prompts the user for manual configuration input.
+
+    Parameters
+    ----------
+    run_name : str, optional
+        The name of the run.
+
+    Returns
+    -------
+    array_configuration : str
+    """
+    at_configs = ["ATs", "small", "medium", "large", "extended"]
+    if run_name:
+        if "UTs" in run_name:
+            array_configuration = "UTs"
+        elif any(config in run_name for config in at_configs):
+            if "small" in run_name:
+                array_configuration = "small"
+            elif "medium" in run_name:
+                array_configuration = "medium"
+            elif "large" in run_name:
+                array_configuration = "large"
+            else:
+                array_configuration = "extended"
+    else:
+        array_configuration = prompt_user("array_configuration",
+                                          at_configs[1:])
+    return array_configuration
+
+
+def parse_run_resolution(run_name: str) -> str:
+    """Parses the run's resolution from string containing it.
+
+    If no match can be found it prompts the user for
+    manual resolution input.
+    """
+    if "LR" in run_name:
+        resolution = "LOW"
+    elif "MR" in run_name:
+        resolution = "MED"
+    elif "HR" in run_name:
+        resolution = "HIGH"
+    else:
+        resolution = prompt_user("resolution", ["LOW", "MED", "HIGH"])
+    return resolution
+
+
+def parse_operational_mode(run_name: str) -> str:
+    """Parses the run's used instrument from string containing it,
+    either MATISSE or GRA4MAT.
+
+    If no match can be found it prompts the user for
+    manual operational mode input.
+    """
+    if "GRA4MAT" in run_name:
+        operational_mode = "gr"
+    elif "MATISSE" in run_name:
+        operational_mode = "st"
+    else:
+        operational_mode = prompt_user("instrument",
+                                       ["matisse", "gra4mat"])
+    return operational_mode
+
+
+def parse_line(parts: str) -> str:
+    """Parses a line from a night plan generated with the `calibrator_find`
+    -tool and returns the objects name.
+
+    Parameters
+    ----------
+    parts : str
+        The line split into its individual components.
+
+    Returns
+    -------
+    target_name : str
+        The target's name.
     """
     target_name_cutoff = len(parts)
     for index, part in enumerate(parts):
@@ -54,9 +157,21 @@ def parse_line(parts: str) -> str:
     return " ".join(parts[1:target_name_cutoff])
 
 
-def parse_groups(section: List):
-    """Parses any combination of a calibrator-science target block into a dictionary
-    containing all the blocks information"""
+def parse_groups(section: List) -> Dict:
+    """Parses any combination of a calibrator-science target block
+    into a dictionary containing the individual blocks' information.
+
+    Parameters
+    ----------
+    section : list
+        A section of a night plan (either a run or a night of a run).
+
+    Returns
+    -------
+    data : Dict
+        The individual science target/calibrator group within a section.
+        Can be for instance, "SCI-CAL" or "CAL-SCI-CAL" or any combination.
+    """
     data = {}
     calibrator_labels = ["name", "order", "tag"]
     current_group, current_science_target = [], None
@@ -113,74 +228,59 @@ def get_file_section(lines: List, identifier: str) -> Dict:
     if not indices:
         indices, labels = [0], ["full_" + identifier]
 
-    sections = [lines[index:] if index == indices[~0] else \
-                  lines[index:indices[i+1]] for i, index in enumerate(indices)]
-    return {labels: sections for (labels, sections) in zip(labels, sections)}
+    sections = [lines[index:] if index == indices[~0] else
+                lines[index:indices[i+1]] for i, index in enumerate(indices)]
+    return dict(zip(labels, sections))
 
 
-def parse_night_plan(night_plan_path: Path,
+# TODO: Add documentation for the night dict in the Returns
+def parse_night_plan(night_plan: Path,
                      run_identifier: Optional[str] = "run",
-                     night_identifier: Optional[str] = "night",
-                     save_path: Optional[Path] = None) -> Dict[str, Any]:
-    """Parses the night plan created with 'calibrator_find.pro' into the
-    individual runs as key of a dictionary, specified by the 'run_identifier'.
-    If no match is found then it parses the whole night to 'run_identifier's
-    or the 'default_key', respectively.
+                     night_identifier: Optional[str] = "night"
+                     ) -> Dict[str, Dict]:
+    """Parses the night plan created with `calibrator_find.pro` into the
+    individual runs as key of a dictionary.
+
+    The parsed runs are specified by the 'run_identifier'. The parsed
+    nights are specified by the 'night_identifier'.
+    If no sections via the 'run_identifier' can be matched, then
+    the full file will be parsed for nights and saved as 'full_run'.
+    The same happens if no match via the 'night_identifier' can be
+    made then it parses all within a run into 'full_night'.
+
 
     Parameters
     ----------
-    night_plan_path: Path
-        The night plan, a (.txt)-file containing the observations for one
-        or more runs
-    run_identifier: str, optional
-        The run-identifier that splits night plan into its runs
-    night_identifier: str, optional
+    night_plan : path
+        The path to the night plan, a (.txt)-file containing
+        the observations for one or more runs.
+    run_identifier : str, optional
+        The run-identifier by which the night plan is split into
+        individual runs.
+    night_identifier : str, optional
         The night-identifier by which the runs are split into the
-        its nights.
-    save_path: bool, optional
-        If this is set to true then it saves the dictionary as
-        'night_plan.yaml', Default is 'False'
+        individual nights.
 
     Returns
     -------
-    night_dict: Dict
-        A dict that contains the <default_search_param> as key and a list
-        containing the sub lists 'sci_lst', 'cal_lst' and 'tag_lst'
+    night_dict : dict
     """
-    night_plan_path = Path(night_plan_path)
-    if night_plan_path.exists():
-        with open(Path(night_plan_path), "r+") as night_plan:
+    night_plan = Path(night_plan)
+    if night_plan.exists():
+        with open(night_plan, "r+", encoding="utf-8") as night_plan:
             lines = night_plan.readlines()
     else:
-        raise FileNotFoundError(f"File {night_plan_path.name} was not found/does not exist!")
+        raise FileNotFoundError(
+            f"File {night_plan.name} was not found/does not exist!")
 
     runs = {}
     for run_id, run in get_file_section(lines, run_identifier).items():
         nights = {}
         for night_id, night in get_file_section(run, night_identifier).items():
             night_content = parse_groups(night)
+
             # HACK: Only add nights that have content
             if night_content:
                 nights[night_id] = night_content
         runs[run_id] = nights
     return runs
-
-    # TODO: Add save functionality back into parser
-    # if save_path:
-        # yaml_file_path = Path(save_path) / "night_plan.yaml"
-        # with open(yaml_file_path, "w+") as night_plan_yaml:
-            # yaml.safe_dump(night_plan_dict, night_plan_yaml)
-        # print(f"Created {yaml_file_path}")
-    # return night_plan_dict
-
-
-if __name__ == "__main__":
-    data_dir = Path("/Users/scheuck/Data/observations/")
-    output_dir = Path("/Users/scheuck/Data/observations/obs")
-    time_slot = data_dir / "P110" / "february_march_2023"
-    night_plan_path = time_slot / "observing_plan_run7_v0.1.txt"
-    for run_id, run_content in parse_night_plan(night_plan_path).items():
-        for night_id, night in run_content.items():
-            lists = read_dict_to_lists(night)
-            print(run_id, night_id)
-            print(lists)
