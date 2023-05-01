9d1b9a01fae30a4bb62fb4d3173438832d44a5b6
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-05-01 12:54:41 +0200

    Add night time determination to `utils`

---
+++ examples/use_utils.py
@@ -0,0 +1,5 @@
+"""This script is meant to give an example on how to use some
+quality of life features of p2obp from the utils backend.
+"""
+from p2obp import calculate_twilight,\
+    calculate_night_lengths, convert_proper_motions
